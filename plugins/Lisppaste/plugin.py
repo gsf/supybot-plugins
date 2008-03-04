4f3ef7883a00576a3fe3f94280f3b76cf7e62f9f
Gabriel Farrell <gsf747@gmail.com>
2008-03-04 22:35:44 +0000

    changed doc string
    git-svn-id: svn://rc98.net/supybot-plugins@23 231ca652-46dc-4159-9403-8d4cb240bb29

--- plugins/Lisppaste/plugin.py
+++ plugins/Lisppaste/plugin.py
@@ -35,7 +35,7 @@ import supybot.callbacks as callbacks
 
 class Lisppaste(callbacks.Privmsg):
     def lisppaste(self, irc, msg, args):
-        """Redirect lisppaste requests to the proper bot
+        """redirects lisppaste requests to the proper bot
         """
         irc.reply('lisppaste: help', prefixNick=False)
 
