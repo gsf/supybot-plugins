9ab3e114ccca11a11fd08f4b37f92607bae26ea2
Gabriel Farrell <gsf747@gmail.com>
2008-03-05 21:20:46 +0000

    some cleanup just to check the post-commit hook
    git-svn-id: svn://rc98.net/supybot-plugins@27 231ca652-46dc-4159-9403-8d4cb240bb29

--- plugins/Traffic/plugin.py
+++ plugins/Traffic/plugin.py
@@ -33,28 +33,30 @@ class Traffic(callbacks.Privmsg):
                 description = result.Description.string
                 last_updated = time.ctime(float(result.UpdateDate.string))
                 image_url = result.ImageUrl.string
-                irc.reply('%s: %s (%s) [%s] <%s>' % (type, title, description, last_updated, image_url), prefixNick=True)
-"""
-<ResultSet xsi:schemaLocation="urn:yahoo:maps http://api.local.yahoo.com/MapsService/V1/TrafficDataResponse.xsd">
-<LastUpdateDate>1204744572</LastUpdateDate>
-<Result type="incident">
-<Title>Lane closed, on US-1 NB at PIERSON AVE</Title>
-<Description>
-SCHEDULED WORK CREW; RIGHT LANE CLOSED; TWO LANES MAINTAINED AND AGAIN FROM GRANDVIEW AVE  EDISON  TO FORD AVE  WOODBRIDGE  UNTIL 2009  MIDDLESEX COUNTY ; STAY LEFT.
-</Description>
-<Latitude>40.532060</Latitude>
-<Longitude>-74.349090</Longitude>
-<Direction>NB</Direction>
-<Severity>2</Severity>
-<ReportDate>1168286345</ReportDate>
-<UpdateDate>1199243627</UpdateDate>
-<EndDate>1262290919</EndDate>
-<ImageUrl>
-http://gws.maps.yahoo.com/mapimage?MAPDATA=tCe4wOd6wXVmWv2oMMBZg2mqo7Wi2BQmoTBCW9RkhYnJppn4exsPvzQO2Be5zMCVDF685wsArOlTSePkiUUGaAsJ1hcsi0wSdmZhV7T.nO.Gz18P40C9fUOAv3Loe9hK_s8P0VlcgbYkDM79BUJvPSPVZTcHQtDiQJFB2flC85apqSTs.q4ASxbbOvv_weC8Dw--&mvt=m?cltype=onnetwork&.intl=us
-</ImageUrl>
-</Result>
-</ResultSet>
-"""
+                irc.reply('%s: %s (%s) [%s] <%s>' % (type, title, description, 
+                    last_updated, image_url), prefixNick=True)
 
 Class = Traffic
+
+# result from yahoo should look like this:
+# 
+# <ResultSet xsi:schemaLocation="urn:yahoo:maps http://api.local.yahoo.com/MapsService/V1/TrafficDataResponse.xsd">
+# <LastUpdateDate>1204744572</LastUpdateDate>
+# <Result type="incident">
+# <Title>Lane closed, on US-1 NB at PIERSON AVE</Title>
+# <Description>
+# SCHEDULED WORK CREW; RIGHT LANE CLOSED; TWO LANES MAINTAINED AND AGAIN FROM GRANDVIEW AVE  EDISON  TO FORD AVE  WOODBRIDGE  UNTIL 2009  MIDDLESEX COUNTY ; STAY LEFT.
+# </Description>
+# <Latitude>40.532060</Latitude>
+# <Longitude>-74.349090</Longitude>
+# <Direction>NB</Direction>
+# <Severity>2</Severity>
+# <ReportDate>1168286345</ReportDate>
+# <UpdateDate>1199243627</UpdateDate>
+# <EndDate>1262290919</EndDate>
+# <ImageUrl>
+# http://gws.maps.yahoo.com/mapimage?MAPDATA=tCe4wOd6wXVmWv2oMMBZg2mqo7Wi2BQmoTBCW9RkhYnJppn4exsPvzQO2Be5zMCVDF685wsArOlTSePkiUUGaAsJ1hcsi0wSdmZhV7T.nO.Gz18P40C9fUOAv3Loe9hK_s8P0VlcgbYkDM79BUJvPSPVZTcHQtDiQJFB2flC85apqSTs.q4ASxbbOvv_weC8Dw--&mvt=m?cltype=onnetwork&.intl=us
+# </ImageUrl>
+# </Result>
+# </ResultSet>
 
