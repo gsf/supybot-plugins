bfb4fc6998b156b67045eb5b062529c9ac289289
Michael J. Giarlo <leftwing@alumni.rutgers.edu>
2008-03-07 14:28:54 +0000

    added some tests
    git-svn-id: svn://rc98.net/supybot-plugins@68 231ca652-46dc-4159-9403-8d4cb240bb29

--- plugins/Traffic/test.py
+++ plugins/Traffic/test.py
@@ -7,6 +7,8 @@ class TrafficTestCase(PluginTestCase):
         self.assertNotError('traffic 22201')
         self.assertNotError('traffic Wilson Blvd., 22201')
         self.assertNotError('traffic 100 Hiram Sq., New Brunswick, NJ, 08901')
+        # I'm not sure these throw actual errors in the plugin
         self.assertError('traffic foobar, bazquux')
-
+        self.assertError('traffic --maps')
+        self.assertError('traffic')
 
