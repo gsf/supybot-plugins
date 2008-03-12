bc314ed7365756958daa03a416f1c5342c7e5c83
Gabriel Farrell <gsf747@gmail.com>
2008-03-12 18:34:43 +0000

    committing LibraryThing
    git-svn-id: svn://rc98.net/supybot-plugins@84 231ca652-46dc-4159-9403-8d4cb240bb29

---
+++ edsu-plugins/LibraryThing/__init__.py
@@ -0,0 +1,12 @@
+"""
+Do stuff with LibraryThing.
+"""
+
+import supybot
+import supybot.world as world
+import config
+import plugin
+reload(plugin) # In case we're being reloaded.
+
+Class = plugin.Class
+configure = config.configure
