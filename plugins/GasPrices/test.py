b061d7e06dfb406d9a82ca895db1c8affa58db2b
Michael J. Giarlo <leftwing@alumni.rutgers.edu>
2008-03-12 15:35:22 +0000

    debugging numresults
    git-svn-id: svn://rc98.net/supybot-plugins@80 231ca652-46dc-4159-9403-8d4cb240bb29

--- plugins/GasPrices/test.py
+++ plugins/GasPrices/test.py
@@ -4,5 +4,6 @@ class GasPricesTestCase(PluginTestCase):
     plugins = ('GasPrices',)
     def testGasPrices(self):
         self.assertNotError('gasprices 22201')
+        self.assertNotError('gasprices 08901 3')
         self.assertError('gasprices')
 
