5bd48f240066dc0c07e477efddd80003b2e2b601
Michael J. Giarlo <leftwing@alumni.rutgers.edu>
2008-03-18 20:32:44 +0000

    fix regex
    git-svn-id: svn://rc98.net/supybot-plugins@106 231ca652-46dc-4159-9403-8d4cb240bb29

--- plugins/Translators/plugin.py
+++ plugins/Translators/plugin.py
@@ -10,7 +10,7 @@ class Translators(callbacks.Privmsg):
         """translates text into a Canadian dialect
         """
         text = ' '.join(args)
-        irc.reply(re.sub(r'out(\b)', 'oot', text) + " eh?", prefixNick=True)
+        irc.reply(re.sub(r'out(\b)', 'oot', text) + "  eh?", prefixNick=True)
 
 Class = Translators
 
