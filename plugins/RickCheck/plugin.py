c7fd87f6390188dc02f1f148bc5c71d1ec79d2f6
Michael J. Giarlo <leftwing@alumni.rutgers.edu>
2008-04-04 12:24:31 +0000

    recursive meta refresh detection refactor
    git-svn-id: svn://rc98.net/supybot-plugins@133 231ca652-46dc-4159-9403-8d4cb240bb29

--- plugins/RickCheck/plugin.py
+++ plugins/RickCheck/plugin.py
@@ -8,46 +8,72 @@ from BeautifulSoup import BeautifulSoup
 from urllib2 import build_opener, HTTPError
 import re
 
-def rickroll_detect(url):
+def __validate_url(url):
     doc = None
     soup = None
-    title = ''
-    
+    ua = 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.8.1.11) Gecko/20071204 Ubuntu/7.10 (gutsy) Firefox/2.0.0.13'
     opener = build_opener()
+    opener.addheaders = [('User-Agent', ua)]
     
     try:
         doc = opener.open(url)
     except HTTPError, e:
-        return 'http error %s for %s' % (e.code, url)
+        __reply('http error %s for %s' % (e.code, url))
     except:
-        return 'bad url: %s' % url
+        __reply('bad url: %s' % url)
     
     doc = doc.read()
     try:
         soup = BeautifulSoup(doc)
     except:
-        return 'could not parse %s' % url
+        __reply('could not parse %s' % url)
+
+    return soup
+
     
+def __reply(message):
+    self.irc.reply(message, prefixNick=True)
+    exit()
+
+
+def __detect_rickroll(soup):
+    title = ''
     try:
         title = soup.find("title").string
     except:
         pass
 
     rickex = re.compile(r'rick.*roll', re.IGNORECASE | re.DOTALL)
-    if rickex.search(title) or rickex.search(doc):
-        return 'DANGER: RickRoll detected in %s' % url
-    
+    return rickex.search(title) or rickex.search(str(soup))
+
+def __detect_meta(soup):
     meta = soup.find("meta", { "http-equiv" : "refresh" })
+
     if meta:
         url_regex = re.compile(r'url\s*=\s*(.+)', re.IGNORECASE)
         match = url_regex.search(meta['content'])
         if match:
-            rickroll_detect(match.groups()[0])
+            return match.groups()[0]
 
-    return 'no RickRoll detected in %s' % url
+    return False
 
+def __rickcheck(url):
+    soup = __validate_url(url)
+    if __detect_rickroll(soup):
+        __reply('DANGER: RickRoll detected in %s' % url)
+        
+    meta_url = __detect_meta(soup)
+    if not meta_url:
+        __reply('no RickRoll detected in %s' % url)
+    else:
+        __rickcheck(meta_url)
 
 class RickCheck(callbacks.Privmsg):
+    def __init__(self, irc):
+        self.__parent = super(RickCheck, self)
+        self.__parent.__init__(irc)
+        self.irc = irc
+
     def rickcheck(self, irc, msg, args):
         """<url> (does RickRoll detection on a URL)
         """
@@ -55,7 +81,6 @@ class RickCheck(callbacks.Privmsg):
             irc.reply('usage: rickcheck <url>', prefixNick=True)
             return
         url = args.pop()
-        irc.reply(rickroll_detect(url), prefixNick=True)
+        __rickcheck(url)
 
 Class = RickCheck
-
