0b8a8196df84296c6e28fa202f88ffcfcdfada2a
Michael B. Klein <mbklein@gmail.com>
2008-04-22 19:00:13 +0000

    Added Lolz plugin
    git-svn-id: svn://rc98.net/supybot-plugins@171 231ca652-46dc-4159-9403-8d4cb240bb29

---
+++ plugins/Lolz/plugin.py
@@ -0,0 +1,59 @@
+###
+# Copyright (c) 2008, Michael B. Klein
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+#   * Redistributions of source code must retain the above copyright notice,
+#     this list of conditions, and the following disclaimer.
+#   * Redistributions in binary form must reproduce the above copyright notice,
+#     this list of conditions, and the following disclaimer in the
+#     documentation and/or other materials provided with the distribution.
+#   * Neither the name of the author of this software nor the name of
+#     contributors to this software may be used to endorse or promote products
+#     derived from this software without specific prior written consent.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+###
+
+import supybot.utils as utils
+from supybot.commands import *
+import supybot.plugins as plugins
+import supybot.ircutils as ircutils
+import supybot.callbacks as callbacks
+from urllib import urlencode
+from urllib2 import urlopen
+from BeautifulSoup import BeautifulSoup, SoupStrainer
+
+
+class Lolz(callbacks.Plugin):
+    pass
+
+    def lolz(self, irc, msg, args):
+        """<phrase>
+        Translates English into Lolcat using http://speaklolcat.com/
+        """
+        phrase = (" ".join(args))
+        query = urlencode({ 'from' : phrase })
+        url = "http://speaklolcat.com/?" + query
+        response = urlopen(url)
+        soup = BeautifulSoup(response, convertEntities='html')
+        lol = soup.findAll('input',{'name' : 'to'})[0]['value']
+        irc.reply(lol, prefixNick=True)
+		
+Class = Lolz
+
+
+# vim:set shiftwidth=4 tabstop=4 expandtab textwidth=79:
