2837d2caa985fed606abe9ae7f39d6e1fe16f58b
Jay Luker <lbjay@reallywow.com>
2008-04-22 18:43:08 +0000

    zen output tweak
    git-svn-id: svn://rc98.net/supybot-plugins@168 231ca652-46dc-4159-9403-8d4cb240bb29

--- edsu-plugins/Assorted/plugin.py
+++ edsu-plugins/Assorted/plugin.py
@@ -641,6 +641,7 @@ class Assorted(callbacks.Privmsg):
         soup = BeautifulSoup(xml)
         zen = soup.findAll('p')[1].string.strip()
         who = soup.find('span', "who").string.strip()
+        who = who.replace(' - ', '')
         irc.reply("%s: %s" % (who, zen), to=msg.nick, prefixNick=False)
 
 
