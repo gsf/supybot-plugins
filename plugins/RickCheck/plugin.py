e932d65105439186ab49f9dde1fdb6795b292db2
Jay Luker <lbjay@reallywow.com>
2008-04-27 21:14:03 +0000

    adding snarfaliciousness to RickCheck
    git-svn-id: svn://rc98.net/supybot-plugins@183 231ca652-46dc-4159-9403-8d4cb240bb29

--- plugins/RickCheck/plugin.py
+++ plugins/RickCheck/plugin.py
@@ -3,56 +3,133 @@ from supybot.commands import *
 import supybot.plugins as plugins
 import supybot.ircutils as ircutils
 import supybot.callbacks as callbacks
+import logging
 
-from BeautifulSoup import BeautifulSoup
-from urllib2 import build_opener, HTTPError
+from urllib2 import urlopen, urlparse, Request, build_opener, HTTPError
+from urllib import urlencode
+from urlparse import urlparse
+import time
+from BeautifulSoup import BeautifulSoup, StopParsing
 import re
 
-class RickCheck(callbacks.Privmsg):
-    def rickcheck(self, irc, msg, args):
-        """<url> (does RickRoll detection on a URL)
+logger = logging.getLogger('supybot')
+
+class RickCheck(callbacks.PluginRegexp,callbacks.Plugin):
+    regexps = ['ricksnarf']
+
+    def __init__(self,irc):
+        self.__parent = super(RickCheck, self)
+        self.__parent.__init__(irc)
+        self._guard_up = False
+
+
+    def rickguard(self, irc, msg, args, cmd):
         """
-        if len(args) != 1:
-            irc.reply('usage: rickcheck <url>', prefixNick=True)
-            return
-        url = args.pop()
-        opener = build_opener()
+        <on|off> : enable or disable automatic rickroll detection
+        """
+        if cmd == 'on':
+            self._guard_up = True
+            irc.reply("RickGuard enabled!")
+        elif cmd == 'off':
+            self._guard_up = False
+            irc.reply("RickGuard disabled!")
+        else:
+            if self._guard_up:
+                status = 'enabled'
+            else:
+                status = 'disabled'
+            irc.reply("Rickguard status: %s" % status)
+        return
 
-        doc = None
+    rickguard = wrap(rickguard, [optional('text')])
+
+    def rickcheck(self, irc, msg, args, url):
+        """<url> : does RickRoll detection on a URL
+        """
+        parsed_url = urlparse(url)
+        if not parsed_url.hostname:
+            irc.reply("That doesn't even look like a URL"); return
+
         try:
-            doc = opener.open(url)
+            score = self._rickscore(parsed_url)
+        except Exception, e:
+            irc.reply(e.message)
+            return
+
+        if (score >= 80):
+            irc.reply('DANGER: RickRoll attempt in %s' % url)
+        elif (score >= 20):
+            irc.reply('WARNING: -possible- RickRoll attempt in %s' % url)
+        else:
+            irc.reply('no RickRoll detected in %s' % url)
+        return
+
+    rickcheck = wrap(rickcheck, ['text'])
+
+    def _rickscore(self, parsed_url, score=0):
+
+        if parsed_url.hostname.find('youtube') != -1:
+            score += 20
+
+        url = parsed_url.geturl()
+        try:
+            soup = self._url2soup(url)
         except HTTPError, e:
-            irc.reply('http error %s for %s' % (e.code, url), prefixNick=True)
-            return
-        except:
-            irc.reply('bad url: %s' % url, prefixNick=True)
-            return
+            raise Exception, 'http error %s for %s' % (e.code, url)
+        except (StopParsing, UnicodeDecodeError), e:
+            raise Exception, 'parsing error "%s" for %s' % (e.reason, url)
+        except Exception, e:
+            logger.info('Got unexpected exception: %s, %s' % (e.__class__, e.message))
+            raise Exception, 'something went wrong (bad url?): %s' % e
 
-        doc_str = doc.read()
-        soup = None
-        try:
-            soup = BeautifulSoup(doc_str)
-        except:
-            irc.reply('could not parse %s' % url, prefixNick=True)
-            return
-
-        title = ''
-        if 'html' in doc.headers.type:
-            try:
-                title = soup.find("title").string
-            except:
-                pass
-
+        title = soup.find("title").string or ''
         rickex = re.compile(r'.*rick.*roll.*', re.IGNORECASE | re.DOTALL)
-        if rickex.match(title) or rickex.match(doc_str):
-            irc.reply('DANGER: RickRoll detected in %s' % url, prefixNick=True)
-            return
+
+        if rickex.match(title):
+            score += 40
+        if rickex.match(soup.__str__()):
+            score += 30
         meta = soup.find("meta", { "http-equiv" : "refresh" })
         if meta:
-            irc.reply('WARNING: -possible- RickRoll attempt in %s' % url, prefixNick=True)
-            return
-        irc.reply('no RickRoll detected in %s' % url, prefixNick=True)
+            score += 30
+        return score
 
+    def ricksnarf(self, irc, msg, match):
+        r"(https?)://[-\w.]+\.[^\s]*"
+        url = match.group(0)
+
+        if not self._guard_up:
+            return
+            
+        parsed_url = urlparse(url)
+        if not parsed_url.hostname:
+            return
+
+        try:
+            score = self._rickscore(url)
+        except:
+            return
+        if score > 20:
+            irc.reply('WARNING: %s may be attempting a RickRoll' % msg.nick, prefixNick=False)
+        return
+
+    def _url2soup(self, url, qsdata={}, postdata=None, headers={}):
+        """
+        Fetch a url and BeautifulSoup-ify the returned doc
+        """
+        logger.info("Fetching: %s" % url)
+        ua = 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.8.1.11) Gecko/20071204 Ubuntu/7.10 (gutsy) Firefox/2.0.0.11'
+        headers.update({'User-Agent': ua})
+        params = urlencode(qsdata)
+        if params:
+            if '?' in url:
+                url = "%s&%s" % (url,params)
+            else:
+                url = "%s?%s" % (url,params)
+        req = Request(url,postdata,headers)
+        doc = urlopen(req)
+        data = doc.read()
+        soup = BeautifulSoup(data)
+        return soup
 
 Class = RickCheck
-
