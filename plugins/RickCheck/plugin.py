0945e922a6f6f9e5bd288c40d92e985a6495cc42
Ed Summers <ehs@pobox.com>
2008-04-28 15:49:19 +0000

    more debug logging
    git-svn-id: svn://rc98.net/supybot-plugins@211 231ca652-46dc-4159-9403-8d4cb240bb29

--- plugins/RickCheck/plugin.py
+++ plugins/RickCheck/plugin.py
@@ -91,8 +91,7 @@ class RickCheck(callbacks.PluginRegexp,callbacks.Plugin):
         return score
 
     def ricksnarf(self, irc, msg, match):
-        r"(?<!^@rickcheck ).*(https?://[-\w.]+\.[^\s]*)"
-        self.log.info(match.group(0))
+        r"(?<!^@rickcheck )(https?://[-\w.]+\.[^\s]*)"
         url = match.group(1)
         self.log.info(url)
 
