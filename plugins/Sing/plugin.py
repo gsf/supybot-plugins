bf8f376a6d807a2f12dc65077e735db694912c20
Jay Luker <lbjay@reallywow.com>
2008-05-02 12:49:29 +0000

    entity escaping; allow for singing entire song
    git-svn-id: svn://rc98.net/supybot-plugins@227 231ca652-46dc-4159-9403-8d4cb240bb29

--- plugins/Sing/plugin.py
+++ plugins/Sing/plugin.py
@@ -5,8 +5,11 @@ import supybot.plugins as plugins
 import supybot.ircutils as ircutils
 import supybot.callbacks as callbacks
 import logging
+from htmlentitydefs import name2codepoint
 
-from BeautifulSoup import BeautifulSoup
+import re
+
+from BeautifulSoup import BeautifulSoup, StopParsing
 import re
 from urllib import urlencode
 from urllib2 import Request, build_opener, HTTPError
@@ -14,58 +17,86 @@ from random import randint
 
 logger = logging.getLogger('supybot')
 
-class Sing(callbacks.Plugin):
-    """Add the help for "@plugin help Sing" here
-    This should describe *how* to use this plugin."""
-    threaded = True
+def random_title(artist):
+    searchurl = 'http://lyricsfly.com/search/search.php'
+    postdata = urlencode({'sort': 1, 'options': 2, 'keywords': artist})
+    soup = url2soup(searchurl,{},postdata)
+    results = [a.string for a in soup.findAll('a', href=re.compile('^view.php'))]
+    logger.info('num results: %d' % len(results))
+    try:
+        ret = results[randint(0, len(results) - 1)]
+        return ret
+    except:
+        return 
 
-    def _url2soup(self, url, qsdata={}, postdata=None, headers={}):
-        """
-        Fetch a url and BeautifulSoup-ify the returned doc
-        """
-        ua = 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.8.1.11) Gecko/20071204 Ubuntu/7.10 (gutsy) Firefox/2.0.0.11'
-        headers.update({'User-Agent': ua})
-        params = urlencode(qsdata)
-        if params:
-            if '?' in url:
-                url = "%s&%s" % (url,params)
-            else:
-                url = "%s?%s" % (url,params)
-        logger.info('fetching %s, %s' % (url,postdata))
-        req = Request(url,postdata,headers)
-        opener = build_opener()
-        doc = opener.open(req)
-        soup = BeautifulSoup(doc.read())
-        return soup
+def url2soup(url, qsdata={}, postdata=None, headers={}):
+    """
+    Fetch a url and BeautifulSoup-ify the returned doc
+    """
+    ua = 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.8.1.11) Gecko/20071204 Ubuntu/7.10 (gutsy) Firefox/2.0.0.11'
+    headers.update({'User-Agent': ua})
+    params = urlencode(qsdata)
+    if params:
+        if '?' in url:
+            url = "%s&%s" % (url,params)
+        else:
+            url = "%s?%s" % (url,params)
+    logger.info('fetching %s, %s' % (url,postdata))
+    req = Request(url,postdata,headers)
+    opener = build_opener()
+    doc = opener.open(req)
+    data = doc.read()
+    data = data.replace("\xa0", "")
+    soup = BeautifulSoup(data, convertEntities=['html','xml'])
+    return soup
+
+def htmlentitydecode(s):
+    return re.sub('&(%s);' % '|'.join(name2codepoint), 
+        lambda m: unichr(name2codepoint[m.group(1)]), s)
+
+class Sing(callbacks.Plugin):
+    """
+    Usage: sing artist [:title] [:line num]
+    """
+    threaded = True
 
     def sing(self, irc, msg, args, input):
         """
-        Fetches lyrics from the lyricsfly.com api --
-        Usage: sing artist [:title] [:line num]
+        Fetches lyrics from the http://lyricsfly.com api --
+        Usage: sing artist [:title] [:line num] --
         Example: @sing bon jovi : wanted dead or alive
         """
 
-        line_idx = None
+        start_line = None
         try: 
-            artist, title, line_idx = map(lambda x: x.strip(), re.split('[:\-]', input))
-            line_idx = int(line_idx)
+            artist, title, start_line = map(lambda x: x.strip(), re.split('[:\-]', input))
+            if start_line == '*':
+                start_line = -1
+            else:
+                start_line = int(start_line)
+            logger.info('%s, %s, %d' % (artist, title, start_line))
         except:
             try:
                 artist, title = map(lambda x: x.strip(), re.split('[:\-]', input))
+                logger.info('%s, %s' % (artist, title))
             except:
                 artist = input
                 try:
-                    title = self._random_title(artist)
+                    title = random_title(artist)
+                    logger.info('%s, random title: %s' % (artist, title))
                 except Exception, e:
                     irc.reply('Got exception %s: %s when searching for songs by %s' \
                     % (e.__class__, e, artist), prefixNick=True); 
                     return
 
+        if title == '*':
+            title = random_title(artist)
+
         lyricsurl = 'http://lyricsfly.com/api/api.php?i=6b03519c191925472'
         qsdata = {'a': artist, 't': title}
 
         try:
-            soup = self._url2soup(lyricsurl, qsdata)
+            soup = url2soup(lyricsurl, qsdata)
         except HTTPError, e:
             irc.reply('http error %s for %s' % (e.code, lyricsurl), prefixNick=True); return
         except StopParsing, e:
@@ -78,59 +109,46 @@ class Sing(callbacks.Plugin):
         if not song('id'):
             irc.reply('No results for "%s"' % input); return
         lyrics = song.tx.string.replace('[br]','\n')
+        lyrics = htmlentitydecode(lyrics)
 
         if re.search('instrumental', lyrics, re.I):
             irc.reply("hums %s by %s" % (song.tt.string, song.ar.string),
                 prefixNick=False, action=True)
             return
 
-#        lyrics = lyrics.replace('\r',' ')
-#        stanzas = lyrics.split('\n\n')
-
-        # if song title is in the lyrics, narrow down to just those stanzas
         titlematch = re.compile(title, re.I | re.MULTILINE)
-#        title_in_lyrics = titlematch.search(lyrics)
-#        if title_in_lyrics:
-#            stanzas = [s for s in stanzas if titlematch.search(s)]
-
-#        stanza = stanzas[randint(0, len(stanzas) - 1)]
-        lines = re.split('[\n\r]+', lyrics) #stanza.split("\n")
-        lines = [l for l in lines if 'lyricsfly' not in l]
-        lines = [l for l in lines if not re.search('written by', l, re.I)]
+        lines = re.split('[\n\r]+', lyrics) 
+        meta = ['lyricsfly','written by','chorus']
+        for m in meta:
+            lines = [l for l in lines if not re.search(m, l, re.I)]
        
-        if not line_idx:
+        if not start_line:
             try:
-                line_idx = [titlematch.search(x) and True for x in lines].index(True)
+                start_line = [titlematch.search(x) and True for x in lines].index(True)
+                logger.info('title matched line %d' % start_line)
             except:
                 try:
-                    line_idx = randint(0,len(lines))
+                    start_line = randint(0,len(lines))
+                    logger.info('using random line %d', start_line)
                 except:
                     irc.reply("I got an empty song")
                     return
 
-        if line_idx > 3:
-            resp = lines[line_idx-4:line_idx]
+        end_line = len(lines)
+        if start_line == -1:
+            resp = lines
         else:
-            try:
-                resp = lines[line_idx:line_idx+4]
-            except:
-                resp = lines[line_idx]
-#        irc.reply('%s - %s' %(' / '.join(resp), title), prefixNick=False)
+            end_line = start_line + 4
+            while end_line >= len(lines):
+                end_line -= 1; start_line -= 1
+            resp = lines[start_line:end_line]
+
+        logger.info('start:end = %d:%d' % (start_line, end_line))
+        resp = [l for l in resp if re.search('\S', l)]
+
         irc.reply('%s' % ' / '.join(resp), prefixNick=False)
 
     sing = wrap(sing, ['text'])
-
-    def _random_title(self, artist):
-        searchurl = 'http://lyricsfly.com/search/search.php'
-        postdata = urlencode({'sort': 1, 'options': 2, 'keywords': artist})
-        soup = self._url2soup(searchurl,{},postdata)
-        results = [a.string for a in soup.findAll('a', href=re.compile('^view.php'))]
-        logger.info('num results: %d' % len(results))
-        try:
-            ret = results[randint(0, len(results) - 1)]
-            return ret
-        except:
-            return 
 
 
 Class = Sing
