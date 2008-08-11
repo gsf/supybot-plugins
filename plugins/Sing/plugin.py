0eafb0fdc7446dbba9ec1f5bf490750e4b64284c
Jay Luker <lbjay@reallywow.com>
2008-08-11 19:55:03 +0000

    converted to fetch from lyricwiki; added ability to use pattern match instead of line number
    git-svn-id: svn://rc98.net/supybot-plugins@318 231ca652-46dc-4159-9403-8d4cb240bb29

--- plugins/Sing/plugin.py
+++ plugins/Sing/plugin.py
@@ -18,172 +18,99 @@ from BeautifulSoup import \
 
 logger = logging.getLogger('supybot')
 
-def random_title(artist):
-    searchurl = 'http://lyricsfly.com/search/search.php'
-    postdata = urlencode({'sort': 1, 'options': 2, 'keywords': artist})
-    soup = url2soup(searchurl, {}, postdata, xml=False)
-    results = [a.string for a in soup.findAll('a', href=re.compile('^view.php'))]
-    logger.info('num results: %d' % len(results))
-    try:
-        ret = results[randint(0, len(results) - 1)]
-        return ret.encode('ascii', 'ignore')
-    except:
-        return 
-
-def url2soup(url, qsdata={}, postdata=None, headers={}, xml=True):
-    """
-    Fetch a url and BeautifulSoup-ify the returned doc
-    """
-    ua = 'Zoia/1.0 (Supybot/0.83; Sing Plugin; http://code4lib.org/irc)'
-    headers.update({'User-Agent': ua})
-
-    params = urlencode(qsdata)
-    if params:
-        if '?' in url:
-            url = "%s&%s" % (url,params)
-        else:
-            url = "%s?%s" % (url,params)
-    logger.info('fetching %s, %s' % (url,postdata))
-    req = Request(url,postdata,headers)
-    opener = build_opener()
-    doc = opener.open(req)
-    data = doc.read()
-    if xml:
-        data = re.sub('&#[A-Za-z0-9]+;', '', data)
-        soup = BSS(data, convertEntities=BSS.XML_ENTITIES)
-    else:
-        soup = BS(data, convertEntities=BS.HTML_ENTITIES)
-    return soup
-
 class Sing(callbacks.Plugin):
     """
-    Usage: sing artist [:title|*] [:line|*] 
+    Usage: sing artist [: title] [: * | line | pattern] 
     """
     threaded = True
+
+    def __init__(self,irc):
+        self.__parent = super(Sing, self)
+        self.__parent.__init__(irc)
+        ua = 'Zoia/1.0 (Supybot/0.83; Sing Plugin; http://code4lib.org/irc)'
+        self.server = SOAPProxy('http://lyricwiki.org/server.php')
 
     def sotd(self, irc, msg, args):
         """
         Fetches song of the day from lyricswiki
         """
         try:
-            server = SOAPProxy('http://lyricwiki.org/server.php')
-            song = server.getSOTD()
+            song = self.server.getSOTD()
             title = song['song']
             artist = song['artist']
-            lyrics = song['lyrics']
-            lyrics = re.sub(r'\n\n', ' ** ', lyrics)
-            lyrics = re.sub(r'\n', ' / ', lyrics)
+            lyrics = self.formatlyrics(song['lyrics'])
             irc.reply('%s by %s...' % (title, artist), prefixNick=False)
             time.sleep(2)
             irc.reply(lyrics, prefixNick=False)
             return
         except Exception, e:
-            irc.reply('Error communicating with server: ' + e)
+            logger.info(e)
+            irc.reply('Error communicating with server: ' + e.message)
             return
-
-    # get this here: http://lyricsfly.com/api/
-    API_ID = 'f4c24c1183135cda8-temporary.API.access'
 
     def sing(self, irc, msg, args, input):
         """
-        Fetches lyrics from the http://lyricsfly.com api --
-        Usage: sing artist [:title|*] [:line|*] --
-        Example: @sing bon jovi : wanted dead or alive
+        Usage: sing artist [: title] [: * | line | pattern] --
+        Example: @sing bon jovi : wanted dead or alive --
+        Fetches lyrics from LyricWiki.org
         """
-
-        start_line = None
+        line = None
+        args = map(lambda x: x.strip(), re.split(':', input))
+        
         try: 
-            artist, title, start_line = map(lambda x: x.strip(), re.split('[:\-]', input))
-            if start_line == '*':
-                start_line = -1
-            else:
-                start_line = int(start_line)
-            logger.info('%s, %s, %d' % (artist, title, start_line))
-        except:
+            artist, title, line = args
+            logger.info('%s, %s, %s' % (artist, title, line))
+        except ValueError:
             try:
-                artist, title = map(lambda x: x.strip(), re.split('[:\-]', input))
-                logger.info('%s, %s' % (artist, title))
+                artist, title = args
             except:
-                artist = input
-                try:
-                    title = random_title(artist)
-                    logger.info('%s, random title: %s' % (artist, title))
-                except Exception, e:
-                    irc.reply('Got exception %s: %s when searching for songs by %s' \
-                    % (e.__class__, e, artist), prefixNick=True); 
-                    return
-
-        if title == '*':
-            title = random_title(artist)
-
-        # stoopid lyricsfly
-        artist = artist.replace("'", '%')
-        title = title.replace("'", '%')
-        lyricsurl = 'http://lyricsfly.com/api/api.php?i=%s' % Sing.API_ID
-        qsdata = {'a': artist, 't': title}
+                irc.reply('random song selection not implemented')
+                return
 
         try:
-            soup = url2soup(lyricsurl, qsdata)
-        except HTTPError, e:
-            irc.reply('http error %s for %s' % (e.code, lyricsurl), prefixNick=True); return
-        except StopParsing, e:
-            irc.reply('parsing error %s for %s' % (e.code, lyricsurl), prefixNick=True); return
+            song = self.getsong(artist, title)
+        except Exception, e:
+            logger.info(e)
+            irc.reply('error commuincating with lyricwiki: ' + e.message)
 
-        songs = soup('sg')
-        if not songs:
-            irc.reply('No results for "%s"' % input); return
-            return
-        song = songs[randint(0, len(songs) - 1)]
-        if not song('id'):
-            irc.reply('No results for "%s"' % input); return
-            return
-
-        if re.search(r'expired', str(song.tx)):
-            irc.reply('the dumb lyricsfly temporary API ID has expired')
-            return
-
-        lyrics = song.tx.string.replace('[br]','\n')
-
-        if re.search('instrumental', lyrics, re.I):
-            irc.reply("hums %s by %s" % (song.tt.string, song.ar.string),
-                prefixNick=False, action=True)
-            return
-
-        titlematch = re.compile(title, re.I | re.MULTILINE)
-        lines = re.split('[\n\r]+', lyrics) 
-        meta = ['lyricsfly','written by','chorus']
-        for m in meta:
-            lines = [l for l in lines if not re.search(m, l, re.I)]
-       
-        if not start_line:
-            try:
-                start_line = [titlematch.search(x) and True for x in lines].index(True)
-                logger.info('title matched line %d' % start_line)
-            except:
-                try:
-                    start_line = randint(0,len(lines))
-                    logger.info('using random line %d', start_line)
-                except:
-                    irc.reply("I got an empty song")
-                    return
-
-        end_line = len(lines)
-        if start_line == -1:
-            resp = lines
+        if song['lyrics'] == 'Not found':
+            irc.reply('No lyrics found', prefixNick=True)
         else:
-            end_line = start_line + 4
-            while end_line >= len(lines):
-                end_line -= 1; start_line -= 1
-            resp = lines[start_line:end_line]
+            lyrics = self.formatlyrics(song, line)
+            irc.reply(lyrics, prefixNick=False)
 
-        logger.info('start:end = %d:%d' % (start_line, end_line))
-        resp = ' / '.join([l for l in resp if re.search('\S', l)])
-        resp = resp.encode('ascii', 'ignore')
+    def getsong(self, artist, title):
+        return self.server.getSong(artist, title)
 
-        irc.reply(resp, prefixNick=False)
+    def formatlyrics(self, song, startline=None):
+        lines = re.split(r'[\n\r]+', song['lyrics'])
+        if startline == '*':
+            startline = 0
+            endline = len(lines)
+        else:
+            if not startline or not startline.isdigit():
+                matchstring = startline if startline else song['song']
+                try:
+                    matcher = re.compile(matchstring, re.I | re.M)
+                    endline = [matcher.search(x) and True for x in lines].index(True) + 1
+                    startline = endline - 4
+                    logger.info('%d,%d' % (startline, endline))
+                    while startline < 0:
+                        startline += 1; endline += 1
+                        logger.info('now %d,%d' % (startline, endline))
+                except:
+                    startline = randint(0, len(lines))
+                    endline = startline + 4
+            else:
+                startline = int(startline)
+                endline = startline + 4
+            while endline > len(lines):
+                endline -= 1; startline -= 1
+
+        lines = lines[startline:endline]
+        return ' / '.join([l for l in lines if re.search('\S', l)])
 
     sing = wrap(sing, ['text'])
-
 
 Class = Sing
 
