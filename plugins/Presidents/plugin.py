e10ab11a34ebe0d828654a6268a7859633135cdd
Jay Luker <lbjay@reallywow.com>
2008-08-30 11:49:47 +0000

    added sarahpalin
    git-svn-id: svn://rc98.net/supybot-plugins@328 231ca652-46dc-4159-9403-8d4cb240bb29

--- plugins/Presidents/plugin.py
+++ plugins/Presidents/plugin.py
@@ -6,6 +6,7 @@ import supybot.callbacks as callbacks
 
 from BeautifulSoup import BeautifulSoup
 from urllib2 import build_opener
+from random import randint
 
 class Presidents(callbacks.Privmsg):
     def mccain(self, irc, msg, args):
@@ -31,6 +32,33 @@ class Presidents(callbacks.Privmsg):
         html_str = html.read()
         soup = BeautifulSoup(html_str)
         irc.reply(soup.find('a').string.strip().upper())
+
+    def sarahpalin(self, irc, msg, args):
+        """Taken from http://www.dailykos.com/storyonly/2008/8/29/14238/1838"""
+        choices = """Sarah Palin is your new Segway.
+        Sarah Palin is your New New Coke.
+        Sarah Palin is your new FoxTrax.
+        Sarah Palin is your new Edsel.
+        Sarah Palin is your new Dennis Miller on Monday Night Football.
+        Sarah Palin is your new Nü Metal.
+        Sarah Palin is your new Betamax.
+        Sarah Palin is your new Dunder Mifflin Infinity.
+        Sarah Palin is your new Windows Vista.
+        Sarah Palin is your new OK Cola.
+        Sarah Palin is your new Stone Temple Pilots reunion tour.
+        Sarah Palin is your new Emily's Reasons Why Not.
+        Sarah Palin is your new Ryan Leaf.
+        Sarah Palin is your new Star Wars Episodes I-III, plus the Clone Wars.
+        Sarah Palin is your new Ford Pinto.
+        Sarah Palin is your new Cuil.
+        Sarah Palin is your new Dick Sargent.
+        Sarah Palin is your new XFL.
+        Sarah Palin is your new Apple Newton.
+        Sarah Palin is your new Brett Favre's retirement.
+        Sarah Palin is your new Laserdisc.
+        Sarah Palin is your new Olestra.
+        Sarah Palin is your new fill in the blank game in comments.""".split("\n")
+        irc.reply(choices[randint(0, len(choices) - 1)].strip())
 
 Class = Presidents
 
