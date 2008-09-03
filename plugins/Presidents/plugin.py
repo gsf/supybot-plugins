04db9a2224812190d964d73b834f04a3c9e9dd50
Jay Luker <lbjay@reallywow.com>
2008-09-03 02:33:06 +0000

    aahh, wtf. this shit is too ripe
    git-svn-id: svn://rc98.net/supybot-plugins@335 231ca652-46dc-4159-9403-8d4cb240bb29

--- plugins/Presidents/plugin.py
+++ plugins/Presidents/plugin.py
@@ -34,5 +34,18 @@ class Presidents(callbacks.Privmsg):
         soup = BeautifulSoup(html_str)
         irc.reply(soup.find('a').string.strip().upper())
 
+    def sarahpalin(self, irc, msg, args):
+        """grabs a line from http://sarahpalinisyournewsegway.com/
+        """
+        url = 'http://sarahpalinisyournewsegway.com/'
+        ua = 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.8.1.11) Gecko/20071204 Ubuntu/7.10 (gutsy) Firefox/2.0.0.11'
+        opener = build_opener()
+        opener.addheaders = [('User-Agent', ua)]
+        html = opener.open(url)
+        html_str = html.read()
+        soup = BeautifulSoup(html_str)
+        resp = 'Sarah Palin is your new ' + soup.find('span', 'flash_text').string.strip()
+        irc.reply(resp.upper())
+
 Class = Presidents
 
