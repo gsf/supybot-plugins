9c8fb6c44dccdbe5494d15cc96d094e4f9f19bf0
Dan Scott <denials@gmail.com>
2008-09-26 22:03:26 +0000

    Strip the protocol from URLs because downforeveryoneorjustme.com doesn't
    git-svn-id: svn://rc98.net/supybot-plugins@393 231ca652-46dc-4159-9403-8d4cb240bb29

--- plugins/IsItDown/plugin.py
+++ plugins/IsItDown/plugin.py
@@ -3,6 +3,7 @@ from supybot.commands import *
 import supybot.plugins as plugins
 import supybot.ircutils as ircutils
 import supybot.callbacks as callbacks
+import re
 
 from BeautifulSoup import BeautifulSoup
 from urllib2 import build_opener, HTTPError
@@ -16,6 +17,9 @@ class IsItDown(callbacks.Privmsg):
         ua = 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.8.1.11) Gecko/20071204 Ubuntu/7.10 (gutsy) Firefox/2.0.0.11'
         opener = build_opener()
         opener.addheaders = [('User-Agent', ua)]
+        # strip the protocol because downforeveryoneorjustme.com is
+        # too stupid to do that; we're smarter than that, damnit
+        url = re.sub(r'^.*?://', r'', url)
         try:
             html = opener.open(site + url)
             html_str = html.read()
