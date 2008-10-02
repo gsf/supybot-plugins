56def70ddd00188a2c7c1e406a3f6d00e06a2dce
Mark A. Matienzo <mark@matienzo.org>
2008-10-02 15:09:27 +0000

    added isiticedcoffeeweather
    git-svn-id: svn://rc98.net/supybot-plugins@409 231ca652-46dc-4159-9403-8d4cb240bb29

--- edsu-plugins/Assorted/plugin.py
+++ edsu-plugins/Assorted/plugin.py
@@ -525,6 +525,18 @@ class Assorted(callbacks.Privmsg):
         soup = BeautifulSoup(html_str)
         irc.reply(soup.find('a').string.strip().upper())
 
+    def isiticedcoffeeweather(self,irc,msg,args):
+        if len(args) != 1:
+            irc.reply("usage: isiticedcoffeeweather zipcode")
+            return
+        url = 'http://isiticedcoffeeweather.com/%s' % zipcode
+        ua = 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.8.1.11) Gecko/20071204 Ubuntu/7.10 (gutsy) Firefox/2.0.0.11'
+        opener = build_opener()
+        opener.addheaders = [('User-Agent', ua)]
+        html = opener.open(url)
+        html_str = html.read()
+        soup = BeautifulSoup(html_str)
+        irc.reply(soup.find('h2').string.strip().upper())
 
     def get_text(self,e):
         string = ''
