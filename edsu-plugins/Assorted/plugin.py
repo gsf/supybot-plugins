6d5a879beab867d52fba7eabd990a87e0f45deae
Ed Summers <ehs@pobox.com>
2008-11-12 02:10:13 +0000

    fixing it up
    git-svn-id: svn://rc98.net/supybot-plugins@519 231ca652-46dc-4159-9403-8d4cb240bb29

--- edsu-plugins/Assorted/plugin.py
+++ edsu-plugins/Assorted/plugin.py
@@ -943,8 +943,7 @@ class Assorted(callbacks.Privmsg):
             if li.a:
                 beers.append((li.a['href'], li.a.string))
         beer = beers[randint(0, len(beers))]
-        irc.reply("fills a pint glass with %s, and sends it down the aisle to %s
-            (%s)" % (beer[0], 'john', beer[1]))
+        irc.reply("fills a pint glass with %s, and sends it down the aisle to %s (%s)" % (beer[0], 'john', beer[1]))
 
 
     
