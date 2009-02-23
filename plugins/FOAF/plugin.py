5422098de40d71067b48547e02a32850bc7515f3
Jonathan Brinley <jonathanbrinley@gmail.com>
2009-02-23 22:04:45 +0000

    fixed arg error
    git-svn-id: svn://rc98.net/supybot-plugins@681 231ca652-46dc-4159-9403-8d4cb240bb29

--- plugins/FOAF/plugin.py
+++ plugins/FOAF/plugin.py
@@ -16,13 +16,12 @@ class FOAF(callbacks.Privmsg):
         self.g = Graph()
         self.g.parse('http://rc98.net/zoia.rdf', format="xml")
         super(callbacks.Plugin,self).__init__(irc)
-        
-    def foaf(self, irc, msg, args):
-        if len(args) > 1:
-          if args[0] == 'knows':
-            usernick1 = args[1]
-            usernick2 = args[2]
-            result = self.g.query( 'PREFIX foaf: <http://xmlns.com/foaf/0.1/> SELECT ?uri1 ?uri2 WHERE {<http://www.code4lib.org/id/zoia> foaf:knows ?uri . ?uri1 foaf:nick ?nick1 . ?uri2 foaf:nick ?nick2}', initBindings={'?nick1': usernick1, '?nick2': usernick2} )
+
+    def knows(self, irc, msg, args):
+      usernick1 = args[0]
+      usernick2 = args[0]
+      result = self.g.query( 'PREFIX foaf: <http://xmlns.com/foaf/0.1/> SELECT ?uri1 ?uri2 WHERE {<http://www.code4lib.org/id/zoia> foaf:knows ?uri . ?uri1 foaf:nick ?nick1 . ?uri2 foaf:nick ?nick2}', initBindings={'?nick1': usernick1, '?nick2': usernick2} )
+      
     def known(self, irc, msg, args):
       usernick = args[0]
       result = self.g.query( 'PREFIX foaf: <http://xmlns.com/foaf/0.1/> SELECT ?uri WHERE {<http://www.code4lib.org/id/zoia> foaf:knows ?uri . ?uri foaf:nick ?nick .}', initBindings={'?nick': usernick} )
@@ -30,7 +29,7 @@ class FOAF(callbacks.Privmsg):
         userURI = list(result)[0][0].__str__()
         irc.reply(usernick+"'s URI is <"+userURI+">", prefixNick=True)
       else:
-        irc.reply("I don't know "+args[1]+"'s URI")
+        irc.reply("I don't know "+usernick+"'s URI")
 
 Class = FOAF
 
