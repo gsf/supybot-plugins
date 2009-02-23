5535a52a2b920ed3d2633716dcf78a6bf8853086
Jonathan Brinley <jonathanbrinley@gmail.com>
2009-02-23 22:23:08 +0000

    added know function
    git-svn-id: svn://rc98.net/supybot-plugins@682 231ca652-46dc-4159-9403-8d4cb240bb29

--- plugins/FOAF/plugin.py
+++ plugins/FOAF/plugin.py
@@ -13,9 +13,12 @@ from rdflib import Literal
 class FOAF(callbacks.Privmsg):
   
     def __init__(self, irc):
-        self.g = Graph()
-        self.g.parse('http://rc98.net/zoia.rdf', format="xml")
-        super(callbacks.Plugin,self).__init__(irc)
+      self.g = Graph()
+      self.g.parse('http://rc98.net/zoia.rdf', format="xml")
+      self.uri = rdflib.URIRef('http://www.code4lib.org/id/zoia')
+      self.FOAF = Namespace('http://xmlns.com/foaf/0.1/')
+      super(callbacks.Plugin,self).__init__(irc)
+      
 
     def knows(self, irc, msg, args):
       usernick1 = args[0]
@@ -23,13 +26,31 @@ class FOAF(callbacks.Privmsg):
       result = self.g.query( 'PREFIX foaf: <http://xmlns.com/foaf/0.1/> SELECT ?uri1 ?uri2 WHERE {<http://www.code4lib.org/id/zoia> foaf:knows ?uri . ?uri1 foaf:nick ?nick1 . ?uri2 foaf:nick ?nick2}', initBindings={'?nick1': usernick1, '?nick2': usernick2} )
       
     def known(self, irc, msg, args):
-      usernick = args[0]
+      if len(args) != 1:
+        usernick = args[0]
+      else:
+        irc.reply("Usage: @known [nick]")
+        return
       result = self.g.query( 'PREFIX foaf: <http://xmlns.com/foaf/0.1/> SELECT ?uri WHERE {<http://www.code4lib.org/id/zoia> foaf:knows ?uri . ?uri foaf:nick ?nick .}', initBindings={'?nick': usernick} )
       if len(result) > 0:
         userURI = list(result)[0][0].__str__()
         irc.reply(usernick+"'s URI is <"+userURI+">", prefixNick=True)
       else:
         irc.reply("I don't know "+usernick+"'s URI")
+        
+    def know(self, irc, msg, args):
+      if len(args) != 1:
+        uri = args[0]
+      else:
+        irc.reply("Usage: @know [URI]")
+        return
+      FOAF = self.FOAF
+      user = rdflib.URIRef(uri)
+      self.g.add((user, rdflib.RDF.type, FOAF['Person']))
+      self.g.add((user, FOAF['nick'], msg.nick))
+      self.g.add((self.uri, FOAF['knows'], user))
+      
+      self.g.serialize('/var/www/rc98.net/zoia.rdf')
 
 Class = FOAF
 
