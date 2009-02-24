4596f994f754f75f48ed84d7974b04913a62b4be
Michael B. Klein <mbklein@gmail.com>
2009-02-24 04:36:55 +0000

    Error checking debugging
    git-svn-id: svn://rc98.net/supybot-plugins@734 231ca652-46dc-4159-9403-8d4cb240bb29

--- plugins/FOAF/plugin.py
+++ plugins/FOAF/plugin.py
@@ -87,13 +87,13 @@ class FOAF(callbacks.Privmsg):
 
       try:
         knows1 = self._knows(userURI1, userURI2)
-      except rdflib.ParseError, e:
+      except ParseError, e:
         irc.reply('Error parsing <' + str(userURI1) + '>: ' + str(e))
         return
 
       try:
         knows2 = self._knows(userURI2, userURI1)
-      except rdflib.ParseError, e:
+      except ParseError, e:
         irc.reply('Error parsing <' + str(userURI2) + '>: ' + str(e))
         return
     
