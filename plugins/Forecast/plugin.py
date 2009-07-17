8a1f25adc3a1912c833a02d0e98ebe0a8f41277a
Jay Luker <lbjay@reallywow.com>
2009-07-17 18:50:52 +0000

    MrDys's productive afternoon
    git-svn-id: svn://rc98.net/supybot-plugins@835 231ca652-46dc-4159-9403-8d4cb240bb29

---
+++ plugins/Forecast/plugin.py
@@ -0,0 +1,31 @@
+import supybot.utils as utils
+from supybot.commands import *
+import supybot.plugins as plugins
+import supybot.ircutils as ircutils
+import supybot.callbacks as callbacks
+import re
+
+from BeautifulSoup import BeautifulSoup
+from urllib2 import build_opener, HTTPError
+
+class Forecast(callbacks.Privmsg):
+    def forecast(self, irc, msg, args):
+        """
+        <zipcode>: Returns the response from http://www.thefuckingweather.com/
+        """
+        zipcode = args[0]
+        site = 'http://www.thefuckingweather.com/?zipcode=%s' % zipcode
+        ua = 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.8.1.11) Gecko/20071204 Ubuntu/7.10 (gutsy) Firefox/2.0.0.11'
+        opener = build_opener()
+        opener.addheaders = [('User-Agent', ua)]
+        try:
+            html = opener.open(site)
+            html_str = html.read()
+            soup = BeautifulSoup(html_str)
+            response = soup.find('div', {'class': 'large'})[0].string.strip()
+            irc.reply(response, prefixNick=True)
+        except:
+            irc.reply("Man, I have no idea; things blew up real good.", prefixNick=True)
+    forecast = wrap(forecast, ['text'])
+
+Class = Forecast
