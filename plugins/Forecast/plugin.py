c2c9faec1ebbacea55776b2e0237b1f5fc48be93
Jay Luker <lbjay@reallywow.com>
2009-07-17 19:20:20 +0000

    replacing degree character
    git-svn-id: svn://rc98.net/supybot-plugins@844 231ca652-46dc-4159-9403-8d4cb240bb29

--- plugins/Forecast/plugin.py
+++ plugins/Forecast/plugin.py
@@ -23,8 +23,7 @@ class Forecast(callbacks.Privmsg):
             html_str = html.read()
             soup = BeautifulSoup(html_str)
             response = soup.find('div', 'large').findAll(text=True)
-            response = ' '.join([x.strip() for x in response])
-            response.replace('&deg;', u'\u00BA')
+            response = ' '.join([x.strip() for x in response]).replace('&deg;', u'\u00BA')
             irc.reply(response, prefixNick=True)
         except:
             irc.reply("Man, I have no idea; things blew up real good.", prefixNick=True)
