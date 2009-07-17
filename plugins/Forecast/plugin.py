4cb760c13335e2b2a3f071d115a9fd72f985c43e
Jay Luker <lbjay@reallywow.com>
2009-07-17 19:17:24 +0000

    fixing BS bullshit
    git-svn-id: svn://rc98.net/supybot-plugins@842 231ca652-46dc-4159-9403-8d4cb240bb29

--- plugins/Forecast/plugin.py
+++ plugins/Forecast/plugin.py
@@ -24,6 +24,7 @@ class Forecast(callbacks.Privmsg):
             soup = BeautifulSoup(html_str)
             response = soup.find('div', 'large').findAll(text=True)
             response = u' '.join([x.strip() for x in response])
+            response.replace('&deg;', u'\u00BA')
             irc.reply(response, prefixNick=True)
         except:
             irc.reply("Man, I have no idea; things blew up real good.", prefixNick=True)
