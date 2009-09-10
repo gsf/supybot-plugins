b42556a216a731dd230b0bb5c2d3b0f45d58df59
Michael J. Giarlo <leftwing@alumni.rutgers.edu>
2009-09-10 14:26:33 +0000

    creating plugin for the genderednames app on freebase
    git-svn-id: svn://rc98.net/supybot-plugins@885 231ca652-46dc-4159-9403-8d4cb240bb29

---
+++ plugins/Gender/plugin.py
@@ -0,0 +1,39 @@
+import supybot.utils as utils
+import supybot.utils.web as web
+from supybot.commands import *
+import supybot.plugins as plugins
+import supybot.ircutils as ircutils
+import supybot.callbacks as callbacks
+import simplejson
+
+API_URL = 'http://genderednames.freebaseapps.com/gender_api?name=%s'
+HEADERS = dict(ua = 'Zoia/1.0 (Supybot/0.83; Gender Plugin; http://code4lib.org/irc)')
+
+class Gender(callbacks.Plugin):
+    threaded = True
+
+    def gender(self, irc, msg, args, name):
+        """<name>
+        
+        Returns gender data on name usage from Freebase:
+        http://genderednames.freebaseapps.com/
+        """
+
+        url = API_URL % name
+        json = web.getUrl(url, headers=HEADERS)
+        response = simplejson.loads(json)
+
+        if not response['total']:
+            irc.reply("The name '%s' was not found on Freebase" % response['name'])
+            return
+
+        female_percentage = response['female'] / response['total']
+        male_percentage = response['male'] / response['total']
+        irc.reply("'%s': %s%% female; %s%% male" % (female_percentage, male_percentage), prefixNick=True)
+
+    gender = wrap(gender, ['text'])
+        
+
+Class = Gender
+
+# vim:set shiftwidth=4 tabstop=4 expandtab textwidth=79:
---
+++ plugins/Gender/test.py
@@ -0,0 +1,7 @@
+from supybot.test import *
+
+class GenderTestCase(PluginTestCase):
+    plugins = ('Gender',)
+
+
+# vim:set shiftwidth=4 tabstop=4 expandtab textwidth=79:
