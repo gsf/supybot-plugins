99d3c50d64a5bbb1af1eeedccf4f3b3c5fa0d9b9
Michael J. Giarlo <leftwing@alumni.rutgers.edu>
2009-09-10 14:35:43 +0000

    formatting percentages better
    git-svn-id: svn://rc98.net/supybot-plugins@887 231ca652-46dc-4159-9403-8d4cb240bb29

--- plugins/Gender/plugin.py
+++ plugins/Gender/plugin.py
@@ -9,6 +9,11 @@ import simplejson
 API_URL = 'http://genderednames.freebaseapps.com/gender_api?name=%s'
 HEADERS = dict(ua = 'Zoia/1.0 (Supybot/0.83; Gender Plugin; http://code4lib.org/irc)')
 
+def percentage(dividend, divisor):
+    """ formats a percentage calculated from two ints """
+    return str((float(dividend) / float(divisor)) * 100)[:4]
+
+
 class Gender(callbacks.Plugin):
     threaded = True
 
@@ -27,9 +32,9 @@ class Gender(callbacks.Plugin):
             irc.reply("The name '%s' was not found on Freebase" % name)
             return
 
-        female_percentage = response['female'] / response['total']
-        male_percentage = response['male'] / response['total']
-        irc.reply("'%s': %s%% female; %s%% male" % (name, female_percentage, male_percentage), prefixNick=True)
+        female_percentage = percentage(response['female'], response['total'])
+        male_percentage = percentage(response['male'], response['total'])
+        irc.reply("'%s': %s%% female; %s%% male (%s)" % (name, female_percentage, male_percentage, response), prefixNick=True)
 
     gender = wrap(gender, ['text'])
         
