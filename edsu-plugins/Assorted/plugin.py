d5a1cde9fc472ca89eeb153355917db3af5aa1ae
Ed Summers <ehs@pobox.com>
2009-10-15 20:59:57 +0000

    like this will work
    git-svn-id: svn://rc98.net/supybot-plugins@924 231ca652-46dc-4159-9403-8d4cb240bb29

--- edsu-plugins/Assorted/plugin.py
+++ edsu-plugins/Assorted/plugin.py
@@ -1170,6 +1170,7 @@ class Assorted(callbacks.Privmsg):
         url = 'http://www.computerhistory.org/tdih'
         if len(args) > 0:
           url += '?setdate=%s' % args[0]
+          irc.reply(url)
         soup = self._url2soup(url)
         try: 
             txt = soup.find('div', id='tdihbody').findAll('p')[1].string
