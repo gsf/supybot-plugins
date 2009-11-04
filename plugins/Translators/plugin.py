c5bd57ab8f9acd737fb3e51a5a1436d43095c640
Michael J. Giarlo <leftwing@alumni.rutgers.edu>
2009-11-04 16:51:15 +0000

    test for no input in @scalia
    git-svn-id: svn://rc98.net/supybot-plugins@960 231ca652-46dc-4159-9403-8d4cb240bb29

--- plugins/Translators/plugin.py
+++ plugins/Translators/plugin.py
@@ -124,9 +124,12 @@ class Translators(callbacks.Privmsg):
 
     def scalia(self, irc, msg, args):
         """<string>
-        random 'dick quote' insertion"""
+        random 'scare' quote insertion"""
         # we do this because other plugins pass a one-element list
         # containing a multi-word string
+        if len(args) != 0:
+            irc.reply("try providing some text, morno")
+            return
         words = []
         token_positions = []
         i = 0
