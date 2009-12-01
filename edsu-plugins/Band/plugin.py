811da339b22c48e8642a3db1ffe7d667940cc436
Michael J. Giarlo <leftwing@alumni.rutgers.edu>
2009-12-01 20:00:14 +0000

    you see what's going on here
    git-svn-id: svn://rc98.net/supybot-plugins@983 231ca652-46dc-4159-9403-8d4cb240bb29

--- edsu-plugins/Band/plugin.py
+++ edsu-plugins/Band/plugin.py
@@ -80,7 +80,7 @@ class Band(callbacks.Privmsg):
         jsonfile.close()
         if len(args) > 1:
             if args[0] == 'add':
-                new_band = u' '.join(args[1:]).strip()
+                new_band = u' '.join([arg.decode('utf8') for arg in args[1:]]).strip()
                 json['bands'].append(new_band)
                 try:
                     jsonfile = open(f, 'w')
@@ -91,7 +91,7 @@ class Band(callbacks.Privmsg):
                 else:
                     irc.reply("Band '%s' added to list" % new_band, prefixNick=True)
             elif args[0] == 'remove':
-                band = u' '.join(args[1:]).strip()
+                band = u' '.join([arg.decode('utf8') for arg in args[1:]]).strip()
                 json['bands'].remove(band)
                 try:
                     jsonfile = open(f, 'w')
@@ -102,10 +102,10 @@ class Band(callbacks.Privmsg):
                 else:
                     irc.reply("Band '%s' removed from list" % band, prefixNick=True)
             elif args[0] == 'search':
-                search_str = u' '.join([arg.decode('utf8') for arg in args[1:]])
-                bands = [band.encode('utf8') for band in json['bands'] if band.lower().find(search_str.lower()) != -1]
+                search_str = u' '.join([arg.decode('utf8') for arg in args[1:]]).strip()
+                bands = [band for band in json['bands'] if band.lower().find(search_str.lower()) != -1]
                 if bands:
-                    irc.reply(u' ; '.join(bands), prefixNick=True)
+                    irc.reply(u' ; '.join([band.decode('utf8') for band in bands]), prefixNick=True)
                 else:
                     irc.reply("No bands found matching '%s'" % search_str, prefixNick=True)
         else:
