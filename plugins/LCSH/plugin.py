68be81ad7b351b99609f7a2bb6516ef2999444b7
Ed Summers <ehs@pobox.com>
2009-12-07 22:36:19 +0000

    trying to fix this up
    git-svn-id: svn://rc98.net/supybot-plugins@999 231ca652-46dc-4159-9403-8d4cb240bb29

--- plugins/LCSH/plugin.py
+++ plugins/LCSH/plugin.py
@@ -1,10 +1,8 @@
 from supybot.commands import *
 import supybot.callbacks as callbacks
 
-from urllib2 import urlopen, Request
-from urllib import urlencode
+from urllib import urlencode, urlopen
 from socket import setdefaulttimeout
-import simplejson
 
 class LCSH(callbacks.Privmsg):
 
@@ -12,57 +10,18 @@ class LCSH(callbacks.Privmsg):
         """lcsh search for headings
         """
         heading = ' '.join(args)
-        headings = self.do_search(heading)
-        results = map(lambda r: "%s <%s>" % (r['pref_label'], r['uri']), headings)
+        headings = self._do_search(heading)
+        results = map(lambda r: "%s <%s>" % (r['pref_label'], r['url']), 
+                      headings)
         if not results:
             irc.reply('sorry no hits for %s, email Sandy Berman' % heading)
         else:
             irc.reply('; '.join(results).encode('utf-8'))
 
-    def lcsh(self, irc, msg, args):
-        """display information for a heading
-        """
-        heading = ' '.join(args)
-        headings = self.do_search(' '.join(args))
-
-        found_heading = None
-        for h in headings:
-            if h['pref_label'].lower() == heading.lower():
-                found_heading = h
-       
-        if not found_heading:
-            irc.reply(("couldn't find concept for %s " + 
-                "try searching with search") % heading)
-            return
-
+    def _do_search(self, heading):
         setdefaulttimeout(60)
-        url = found_heading['uri']
-        json = urlopen(Request(url, None, {'Accept': 'application/json'})).read()
-        concept = simplejson.loads(json)
-        msg = [concept['pref_label']]
-
-        if concept['alt_labels']:
-            msg.append('see from: ' + (', '.join(concept['alt_labels'])))
-
-        self.add_concept_list(concept, 'broader', msg)
-        self.add_concept_list(concept, 'narrower', msg)
-        self.add_concept_list(concept, 'related', msg)
-
-        irc.reply(' ; '.join(msg).encode('utf-8'))
-
-
-    def add_concept_list(self, concept, type, msg):
-        if concept[type]:
-            msg.append((type + ': ' + ', '.join(
-                [
-                  "%s <%s>" % (h['pref_label'], h['uri']) 
-                  for h in concept[type]
-                ])))
-
-    def do_search(self, heading):
-        setdefaulttimeout(60)
-        url = 'http://lcsh.info/search?' + urlencode({'q': heading})
-        json = urlopen(Request(url, None, {'Accept': 'application/json'})).read()
-        return simplejson.loads(json)
+        url = 'http://id.loc.gov/authorities/search?' + 
+              urlencode({'q': heading, 'format': 'json'})
+        return simplejson.loads(urlopen(url).read())
 
 Class = LCSH
