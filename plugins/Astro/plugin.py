cedead958388cbcbb629b13ccbd8dde28f8ac90c
Jay Luker <lbjay@reallywow.com>
2009-12-18 22:32:48 +0000

    new plugin for astro goodies
    git-svn-id: svn://rc98.net/supybot-plugins@1004 231ca652-46dc-4159-9403-8d4cb240bb29

---
+++ plugins/Astro/plugin.py
@@ -0,0 +1,61 @@
+###
+# Copyright (c) 2009, lbjay
+# All rights reserved.
+#
+#
+###
+
+import supybot.utils as utils
+from supybot.commands import *
+import supybot.plugins as plugins
+import supybot.ircutils as ircutils
+import supybot.callbacks as callbacks
+
+import re
+from lxml.html.soupparser import fromstring
+from lxml.cssselect import CSSSelector
+from datetime import datetime
+
+HEADERS = dict(ua = 'Zoia/1.0 (Supybot/0.83; Astro Plugin; http://code4lib.org/irc)')
+
+class Astro(callbacks.Plugin):
+    """Add the help for "@plugin help Astro" here
+    This should describe *how* to use this plugin."""
+    threaded = True
+
+    def pha(self, irc, msg, args):
+        au2miles = 92955887.6
+        url = 'http://www.cfa.harvard.edu/iau/lists/PHACloseApp.html'
+        
+        # stupid astronomers and their stupid <pre> data
+        # example of parsed pha
+        # ('2002 AT4  ',        => object name
+        #  '2511159.67',        => julian date
+        #  '2163 Mar. 22.17',   => calendar date
+        #  '0.05000')           => distance in AU
+        pattern = re.compile('\s*([\(\)\w ]+?  )\s*([\d\.]+)\s*(\d{4} [a-z\.]+\s*[\d\.]+)\s*([\d\.]+)', re.I)
+
+        try:
+            html = web.getUrl(url, headers=HEADERS)
+            tree = fromstring(html)
+            pre = tree.xpath('//pre')[0]
+            lines = pre.split('\n')[3:]
+            lines = [l for l in lines if len(l)]
+            phas = [re.match(pattern, l).groups() for l in lines]
+            phas.sort(lambda a,b: cmp(a[1], b[1]))
+            (name, jd, date, au) = phas[0]                
+            date = date.replace('.', ' ')
+            # the %j is just a placeholder
+            date = dateparse.strptime(date, "%Y %b %d %j")
+            miles = float(au) * au2miles
+            resp = "Object %s will pass within %f miles " + \
+                " of earth on %s" % (name, miles, date.strftime("%c"))
+            irc.reply(resp)
+        except:
+            irc.reply("Huh, that didn't work")
+        
+
+Class = Astro
+
+
+# vim:set shiftwidth=4 softtabstop=4 expandtab textwidth=79:
---
+++ plugins/Astro/test.py
@@ -0,0 +1,14 @@
+###
+# Copyright (c) 2009, lbjay
+# All rights reserved.
+#
+#
+###
+
+from supybot.test import *
+
+class AstroTestCase(PluginTestCase):
+    plugins = ('Astro',)
+
+
+# vim:set shiftwidth=4 tabstop=4 expandtab textwidth=79:
