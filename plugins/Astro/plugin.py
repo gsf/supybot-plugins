8f18e9dd57ff1aed0b8ec53f7280c77a3c349cb5
Jay Luker <lbjay@reallywow.com>
2009-12-19 14:27:05 +0000

    needed to filter out past objects
    git-svn-id: svn://rc98.net/supybot-plugins@1007 231ca652-46dc-4159-9403-8d4cb240bb29

--- plugins/Astro/plugin.py
+++ plugins/Astro/plugin.py
@@ -12,11 +12,12 @@ import supybot.ircutils as ircutils
 import supybot.callbacks as callbacks
 import supybot.utils.web as web
 
-import re
+import re, logging, time
 from lxml.html.soupparser import fromstring
 from lxml.cssselect import CSSSelector
 from datetime import datetime
 
+logger = logging.getLogger('supybot')
 HEADERS = dict(ua = 'Zoia/1.0 (Supybot/0.83; Astro Plugin; http://code4lib.org/irc)')
 
 class Astro(callbacks.Plugin):
@@ -25,8 +26,10 @@ class Astro(callbacks.Plugin):
     threaded = True
 
     def asteroid(self, irc, msg, args):
-        au2miles = 92955887.6
-        url = 'http://www.cfa.harvard.edu/iau/lists/PHACloseApp.html'
+        """Fetch the next Potentially Hazardous Asteroid as
+        reported by the Near Earth Object Program
+        (http://www.cfa.harvard.edu/iau/lists/PHACloseApp.html)
+        """
         
         # stupid astronomers and their stupid <pre> data
         # example of parsed pha
@@ -36,18 +39,35 @@ class Astro(callbacks.Plugin):
         #  '0.05000')           => distance in AU
         pattern = re.compile('\s*([\(\)\w ]+?  )\s*([\d\.]+)\s*(\d{4} [a-z\.]+\s*[\d\.]+)\s*([\d\.]+)', re.I)
 
+        # get now in julian
+        julian_now = (time.time() / 86400.0) + 2440587
+
+        url = 'http://www.cfa.harvard.edu/iau/lists/PHACloseApp.html'
         html = web.getUrl(url, headers=HEADERS)
         tree = fromstring(html)
         pre = tree.xpath('//pre')[0]
         lines = pre.text.split('\n')[3:]
         lines = [l for l in lines if len(l)]
+
+        # match group tuples become our object data 
         phas = [re.match(pattern, l).groups() for l in lines]
-        phas.sort(lambda a,b: cmp(a[1], b[1]))
+        
+        # ignore those earlier than now
+        phas = [p for p in phas if float(p[1]) > julian_now]
+
+        # sort by julian date
+        phas.sort(lambda a,b: cmp(float(a[1]), float(b[1])))
+
+        # grab the next event
         (name, jd, date, au) = phas[0]                
         date = date.replace('.', ' ')
+
         # the %j is just a placeholder
         date = datetime.strptime(date, "%Y %b %d %j")
-        miles = float(au) * au2miles
+
+        # convert AU to miles
+        miles = float(au) * 92955887.6
+
         resp = "Object '%s' will pass within %s miles of earth on %s"
         irc.reply(resp % (name.strip(), miles, date.strftime("%c")))
 
