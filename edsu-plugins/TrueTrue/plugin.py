af3b3cca89cf03947b734c4f79140972ef92509c
Michael J. Giarlo <leftwing@alumni.rutgers.edu>
2010-03-05 18:06:47 +0000

    snarfer changes, adding lolsnarfer
    git-svn-id: svn://rc98.net/supybot-plugins@1068 231ca652-46dc-4159-9403-8d4cb240bb29

--- edsu-plugins/TrueTrue/plugin.py
+++ edsu-plugins/TrueTrue/plugin.py
@@ -4,10 +4,11 @@ import time
 
 class TrueTrue(callbacks.PluginRegexp):
     regexps = ['cassonSnarfer', 'chuckleSnarfer', 'upchuckSnarfer',
-        #'billSnarfer',
-        'coughSnarfer', 'metadataSnarfer', 'crueSnarfer', 'knockSnarfer',
-        'panizziSnarfer', 'opacSnarfer', 'yawnSnarfer', 'callMeSomething',
-        'snortSnarfer', 'sobSnarfer'] #, 'hehSnarfer']
+               'coughSnarfer', 'metadataSnarfer', 'crueSnarfer', 'knockSnarfer',
+               'panizziSnarfer', 'opacSnarfer', 'yawnSnarfer', 'callMeSomething',
+               'snortSnarfer', 'sobSnarfer', 'lolSnarfer'
+               #'billSnarfer', 'hehSnarfer'
+               ] 
 
     def cassonSnarfer(self,irc,msg,match):
         r"true, true"
@@ -16,15 +17,15 @@ class TrueTrue(callbacks.PluginRegexp):
             irc.reply("tsk tsk")
         else:
             irc.reply("chuckles", action=True, prefixNick=False)
-
+            
     def crueSnarfer(self,irc,msg,match):
-      r"crue"
-      irc.reply("turn it up!")
+        r"crue"
+        irc.reply("turn it up!")
 
     def opacSnarfer(self,irc,msg,match):
-      r" opac "
-      if randint(1,10) != 1: return
-      irc.reply("here we go again ...", prefixNick=False)
+        r" opac "
+        if randint(1,10) != 1: return
+        irc.reply("here we go again ...", prefixNick=False)
 
     def metadataSnarfer(self,irc,msg,match):
         r"metadata"
@@ -32,37 +33,38 @@ class TrueTrue(callbacks.PluginRegexp):
         if randint(1,10) != 1: return
         time.sleep(10)
         responses = [
-          'can you tell me more about this metadata stuff?',
-          'i think there is a xslt crosswalk for that',
-          'is that like data about data?',
-          "Don't cry for me, Metadataaaaa!",
-          "i never met a data i did not like too",
-          'can i get a witness?',
-          'Greeks prefer fetadata',
-          'its just data, jeez',
-          'take my metadata, please?',
-          'metadata is for the birds, tweet, tweet, tweet',
-          'can we talk about something else?'
-          'is metadata like xml and stuff?',
-          'infinite recursion error',
-          "And don't get me started about metadata!",
-          "You say metadata \ I say data about data \ let's call the whole thing off",
-          'and on the 8th day god made metadata...and it was good',
-          'Can you shut the hell up for one minute about the metadata?',
-          'Metadata, metadata, metadata!  What about MARC?',
-          'marc is dead, long live ummm...metadata']
+            'can you tell me more about this metadata stuff?',
+            'i think there is a xslt crosswalk for that',
+            'is that like data about data?',
+            "Don't cry for me, Metadataaaaa!",
+            "i never met a data i did not like too",
+            'can i get a witness?',
+            'Greeks prefer fetadata',
+            'its just data, jeez',
+            'take my metadata, please?',
+            'metadata is for the birds, tweet, tweet, tweet',
+            'can we talk about something else?'
+            'is metadata like xml and stuff?',
+            'infinite recursion error',
+            "And don't get me started about metadata!",
+            "You say metadata \ I say data about data \ let's call the whole thing off",
+            'and on the 8th day god made metadata...and it was good',
+            'Can you shut the hell up for one minute about the metadata?',
+            'Metadata, metadata, metadata!  What about MARC?',
+            'marc is dead, long live ummm...metadata']
         irc.reply(responses[randint(0,len(responses))], prefixNick=False)
 
     def chuckleSnarfer(self,irc,msg,match):
         r"chuckles"
         time.sleep(2)
+        if randint(1,5) != 1: return
         irc.reply("true, true", prefixNick=False)
 
     def coughSnarfer(self,irc,msg,match):
         r"coughs"
         time.sleep(2)
         irc.reply("sneezes", action=True, prefixNick=False)
-
+        
     def upchuckSnarfer(self,irc,msg,match):
         r"upchucks"
         time.sleep(2)
@@ -95,23 +97,30 @@ class TrueTrue(callbacks.PluginRegexp):
     	r'^heh$'
     	time.sleep(2)
     	if randint(1,10) == 1: 
-    	     irc.reply("it wasn't that funny...")
+            irc.reply("it wasn't that funny...")
     	else:
-    	     irc.reply("typical...", prefixNick=False)
+            irc.reply("typical...", prefixNick=False)
 
     def callMeSomething(self, irc, msg, match):
-      r'^[Cc]all me (.+?)(?:[.;:]|(?:,?\s*\b(and|if|but)\b)|$)'
-      time.sleep(2)
-      irc.reply("You're %s." % (match.group(1)))
+        r'^[Cc]all me (.+?)(?:[.;:]|(?:,?\s*\b(and|if|but)\b)|$)'
+        time.sleep(2)
+        irc.reply("You're %s." % (match.group(1)))
       
     def sobSnarfer(self, irc, msg, match):
-      r'\bsobs\b'
-      time.sleep(0.5)
-      irc.reply('snorts', action=True, prefixNick=False)
+        r'\bsobs\b'
+        if randint(1,5) != 1: return
+        time.sleep(0.5)
+        irc.reply('snorts', action=True, prefixNick=False)
 
     def snortSnarfer(self, irc, msg, match):
-      r'\bsnorts\b'
-      time.sleep(0.5)
-      irc.reply('sobs', action=True, prefixNick=False)
+        r'\bsnorts\b' 
+        if randint(1,5) != 1: return
+        time.sleep(0.5)
+        irc.reply('sobs', action=True, prefixNick=False)
       
+    def lolSnarfer(self, irc, msg, match):
+        r'\blol[sz]?\b' 
+        time.sleep(0.5)
+        irc.reply('lulz', action=True, prefixNick=False)
+            
 Class = TrueTrue
