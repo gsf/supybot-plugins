03bdb5f65c1a8011d09ef8fdf8c8169a950c32b9
Michael B. Klein <mbklein@gmail.com>
2010-04-22 19:33:26 +0000

    Changed GTranslate to official Google AJAX call; added option to cycle through many languages
    git-svn-id: svn://rc98.net/supybot-plugins@1118 231ca652-46dc-4159-9403-8d4cb240bb29

--- plugins/TranslationParty/plugin.py
+++ plugins/TranslationParty/plugin.py
@@ -35,13 +35,16 @@ import supybot.plugins as plugins
 import supybot.ircutils as ircutils
 import supybot.callbacks as callbacks
 import supybot.registry as registry
+import supybot.utils.web as web
+import supybot.log as log
 import sys
 import time
 from random import randrange
 import simplejson
-from urllib import quote, urlencode
-from urllib2 import urlopen, urlparse, Request, build_opener, HTTPError
+from urllib import urlencode
 from BeautifulSoup import BeautifulStoneSoup
+
+HEADERS = dict(ua = 'Zoia/1.0 (Supybot/0.83; http://code4lib.org/irc)', referer = 'http://irc.code4lib.org/')
 
 class TranslationError(Exception):
     def __init__(self, code, value, url, stack):
@@ -58,29 +61,45 @@ class TranslationParty(callbacks.Plugin):
     This should describe *how* to use this plugin."""
     
     def _translate(self, from_lang, to_lang, text):
-        params = { 'langpair' : '|'.join((from_lang,to_lang)), 'q' : text.encode('utf8'), 
-            'key' : 'notsupplied', 'v' : '1.0', 'nocache' : randrange(0,sys.maxint) }
-        url = 'http://www.google.com/uds/Gtranslate?' + urlencode(params)
-        response = simplejson.loads(urlopen(url).read())
+        params = { 'langpair' : '|'.join((from_lang,to_lang)), 'q' : text.encode('utf8'), 'v' : '1.0' }
+        url = 'http://ajax.googleapis.com/ajax/services/language/translate?' + urlencode(params)
+        log.debug('Retrieving: %s' % (url))
+        doc = web.getUrl(url, headers=HEADERS)
+        log.debug('Response: %s' % (doc))
+        response = simplejson.loads(doc)
         if response['responseStatus'] == 200:
             translation = unicode(BeautifulStoneSoup(response['responseData']['translatedText'],convertEntities=BeautifulStoneSoup.HTML_ENTITIES))
             return translation
         else:
             raise TranslationError(response['responseStatus'], response['responseDetails'], url, None)
     
-    def _party(self, from_lang, to_lang, text, max_translations = 50):
+    def _party(self, langs, text, max_translations = 50):
         delay = self.registryValue('delay')
-        stack = [text]
+        stack = [{'lang': langs[0], 'text': text}]
         def iterate():
-            stack.append(self._translate(from_lang,to_lang,stack[-1]))
-            time.sleep(delay)
-            stack.append(self._translate(to_lang,from_lang,stack[-1]))
-            time.sleep(delay)
+            l = list(reversed(langs))
+            while len(l) > 0:
+                f = l.pop()
+                if len(l) == 0:
+                    t = langs[0]
+                else:
+                    t = l[-1]
+                stack.append({'lang': t, 'text': self._translate(f,t,stack[-1]['text'])})
+                time.sleep(delay)
+                
+        def equilibrium():
+            if len(stack) < (len(langs) * 2):
+                return False
+                
+            for i in range(-1,-(len(langs)+1),-1):
+                if stack[i]['text'] != stack[i-len(langs)]['text']:
+                    return False
+            return True
             
         try:
             iterate()
-            iterate()
-            while (len(stack) < max_translations) and ((stack[-2] != stack[-4]) and (stack[-1] != stack[-3])):
+            
+            while (len(stack) < max_translations) and not equilibrium():
                 iterate()
             return(stack)
         except TranslationError, e:
@@ -88,9 +107,9 @@ class TranslationParty(callbacks.Plugin):
             raise e
             
     def translationparty(self, irc, msg, args, opts, text):
-        """[--lang <iso-code>] [--show <none|one|all>] [--max <int>] [--quiet] <text>
+        """[--lang <iso-code>[,...]] [--show <none|one|all>] [--max <int>] [--quiet] <text>
         Try to find equilibrium in back-and-forth translations of <text>. (Defaults: --lang ja --show none --max 50)"""
-        lang = 'ja'
+        langs = ['ja']
         show = 'none'
         max_translations = 50
         announce = True
@@ -100,7 +119,7 @@ class TranslationParty(callbacks.Plugin):
             if opt == 'debug':
                 debug = True
             if opt == 'lang':
-                lang = arg
+                langs = arg.split(',')
             if opt == 'max':
                 max_translations = arg
             if opt == 'quiet':
@@ -108,24 +127,27 @@ class TranslationParty(callbacks.Plugin):
             if opt == 'show':
                 show = arg
         
+        langs.insert(0,'en')
         try:
-            result = self._party('en', lang, text, max_translations)
+            result = self._party(langs, text, max_translations)
             if announce:
                 if len(result) < max_translations:
                     irc.reply("Equilibrium found!")
                 else:
                     irc.reply("It is doubtful that this phrase will ever reach equilibrium.")
 
+            texts = map(lambda x: x['text'],result)
             if show == 'all':
-                irc.reply(" -> ".join(result).encode('utf8'))
+                irc.reply(" -> ".join(texts).encode('utf8'))
             elif show == 'one':
-                irc.reply(" -> ".join((result[0],result[-1])).encode('utf8'))
+                irc.reply(" -> ".join((texts[0],texts[-1])).encode('utf8'))
             else:
-                irc.reply(result[-1].encode('utf8'))
+                irc.reply(texts[-1].encode('utf8'))
         except TranslationError, e:
             irc.reply(e)
             if debug:
-                irc.reply("Stack: %s" % (" -> ".join(e.stack).encode('utf8')))
+                texts = map(lambda x: '[%s] %s' % (x['lang'],x['text']),e.stack)
+                irc.reply("Stack: %s" % (" -> ".join(texts).encode('utf8')))
                 irc.reply("Last URL: %s" % (e.url))
             
     translationparty = wrap(translationparty, [getopts({'debug':'','lang':'somethingWithoutSpaces','show':("literal", ("none","one","all")),'max':'int','quiet':''}), 'text'])
