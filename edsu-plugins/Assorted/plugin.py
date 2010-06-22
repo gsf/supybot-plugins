1035ce347dece77349d265827054c50745081733
Michael B. Klein <mbklein@gmail.com>
2010-06-22 19:42:11 +0000

    Added @professor
    git-svn-id: svn://rc98.net/supybot-plugins@1155 231ca652-46dc-4159-9403-8d4cb240bb29

--- edsu-plugins/Assorted/plugin.py
+++ edsu-plugins/Assorted/plugin.py
@@ -1414,5 +1414,15 @@ class Assorted(callbacks.Privmsg):
         else:
           irc.reply(who + ": " + text, prefixNick=False)
     shake = wrap(shake, [optional('text')])
+
+    def professor(self, irc, msg, args, first, last):
+        f1 = {'A': 'poopsie', 'B': 'lumpy', 'C': 'buttercup', 'D': 'gidget', 'E': 'crusty', 'F': 'greasy', 'G': 'fluffy', 'H': 'cheeseball', 'I': 'chim-chim', 'J': 'stinky', 'K': 'flunky', 'L': 'boobie', 'M': 'pinky', 'N': 'zippy', 'O': 'goober', 'P': 'doofus', 'Q': 'slimy', 'R': 'loopy', 'S': 'snotty', 'T': 'tulefel', 'U': 'dorkey', 'V': 'squeezit', 'W': 'oprah', 'X': 'skipper', 'Y': 'dinky', 'Z': 'zsa-zsa'}
+        l1 = {'A': 'apple', 'B': 'toilet', 'C': 'giggle', 'D': 'burger', 'E': 'girdle', 'F': 'barf', 'G': 'lizard', 'H': 'waffle', 'I': 'cootie', 'J': 'monkey', 'K': 'potty', 'L': 'liver', 'M': 'banana', 'N': 'rhino', 'O': 'bubble', 'P': 'hamster', 'Q': 'toad', 'R': 'gizzard', 'S': 'pizza', 'T': 'gerbil', 'U': 'chicken', 'V': 'pickle', 'W': 'chuckle', 'X': 'tofu', 'Y': 'gorilla', 'Z': 'stinker'}
+        l2 = {'A': 'head', 'B': 'mouth', 'C': 'face', 'D': 'nose', 'E': 'tush', 'F': 'breath', 'G': 'pants', 'H': 'shorts', 'I': 'lips', 'J': 'honker', 'K': 'b utt', 'L': 'brain', 'M': 'tushie', 'N': 'chunks', 'O': 'hiney', 'P': 'biscuits', 'Q': 'toes', 'R': 'buns', 'S': 'fanny', 'T': 'sniffer', 'U': 'sprinkles', 'V': 'kisser', 'W': 'squirt', 'X': 'humperdinck', 'Y': 'brains', 'Z': 'juice'}
+        fname = f1[first[2].upper()]
+        lname = l1[last[1].upper()] + l2[last[3].upper()]
+        name = ('%s %s' % (fname, lname)).title()
+        irc.reply(name.encode('utf-8'), prefixNick=True)
+    professor = wrap(professor, ['somethingWithoutSpaces','somethingWithoutSpaces'])
     
 Class = Assorted
