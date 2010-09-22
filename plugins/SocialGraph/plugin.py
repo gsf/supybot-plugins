da4e0fe78a976d9431eb4df91f1ccd17ec0f551a
Ed Summers <ehs@pobox.com>
2010-09-22 15:19:03 -0400

    trying to write a modest plugin to work with google social graph

---
+++ plugins/SocialGraph/plugin.py
@@ -0,0 +1,28 @@
+import json
+import urllib
+
+import supybot.utils
+import supybot.callbacks
+
+class SocialGraph(supybot.callbacks.Plugin):
+    """Flimsy support for Google's Social Graph API.
+    """
+
+    def otherme(self, irc, msg, args, uri):
+        """Ask google's social graph api about a particular url or email.
+        """
+        if not uri:
+              irc.reply("please supply a URL or email address!")
+              return
+
+        profile = self._get_profile()
+        irc.reply(', '.join(profile.keys()))
+
+    lookup = supybot.utils.wrap(lookup, [optional('text')])
+
+    def _profile(self, uri):
+        url = "http://socialgraph.apis.google.com/otherme?" + \
+            urllib.urlencode({'q': uri})
+        return json.loads(urllib.urlopen(url).read())
+      
+Class = SocialGraph 
