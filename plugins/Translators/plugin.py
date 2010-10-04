145c048fc0cf42ccfdac29b5e0ff252f5d30a7f5
Dan Scott <gitorious@coffeecode.net>
2010-10-04 16:55:35 -0400

    Fix (?) problem with /(\w)or/$1our/ canuck substitution
    Tempted to embed a Perl parser to run the regex through a sane language,
    but go with what ya got. (Just kidding, Python, I love you in a sick
    and creepy fashion).

--- plugins/Translators/plugin.py
+++ plugins/Translators/plugin.py
@@ -23,7 +23,7 @@ class Translators(callbacks.Privmsg):
         """
         text = ' '.join(args).strip()
         text = re.sub(r'z', 'zed', text)
-        text = re.sub(r'(\w)or', '\1our', text)
+        text = re.sub(r'(\w)or', r'\1our', text)
         text = re.sub(r'gray', 'gray', text)
         text = re.sub(r'catalog(?![ui])', 'catalogue\1', text)
         # commenting these out for now since they make output from Weather ugly
