24ccd08f367a404c8ad73286fcf4009593f00ada
Michael J. Giarlo <leftwing@alumni.rutgers.edu>
2010-12-01 11:05:30 -0500

    zalgo, now with random crazy characters

--- plugins/Translators/plugin.py
+++ plugins/Translators/plugin.py
@@ -330,7 +330,17 @@ class Translators(callbacks.Privmsg):
                 zalgo_threshold = 5
         zalgo_chars = [unichr(i) for i in range(0x0300, 0x036F + 1)]
         zalgo_chars.extend([u'\u0488', u'\u0489'])
-        source = text.decode('utf8').upper()
+        random_extras = [unichr(i) for i in range(0x0FBE, 0x0FD4 + 1)]
+        random_extras.extend([unichr(i) for i in range(0x0F00, 0x0F17 + 1)])
+        random_extras.extend([unichr(i) for i in range(0x1D023, 0x1D045 + 1)])
+        def insert_randoms(text):
+            newtext = []
+            for char in text:
+                newtext.append(char)
+                if randint(1,5) == 1:
+                    newtext.append(choice(random_extras))
+            return u''.join(newtext)
+        source = insert_randoms(text.decode('utf8').upper())
         zalgoized = []
         for letter in source:
             zalgoized.append(letter)
