de469c51e45d85e3c982ac928bcb891478b8b76e
Mark A. Matienzo <mark@matienzo.org>
2011-10-27 12:52:34 -0400

    h8 u python whitespace

--- plugins/Translators/plugin.py
+++ plugins/Translators/plugin.py
@@ -311,11 +311,11 @@ class Translators(callbacks.Privmsg):
             words = (' '.join(words)).split()
         r1 = u'( ಠ_ಠ) ' + choice(intro) + u' ' + words[0]
         irc.reply(response.encode('utf-8', 'ignore'))
-		irc.reply(u'( ಠ_ಠ)>--■-■'.encode('utf-8','ignore'))
-		r2 = u'(-■_■) ' + ' '.join(words[1:])
-		irc.reply(r2.response(encode('utf-8', 'ignore')))
-		irc.reply('YEAAAAAAAAAAAAAAAAAAAAAAH')
-    
+        irc.reply(u'( ಠ_ಠ)>--■-■'.encode('utf-8','ignore'))
+        r2 = u'(-■_■) ' + ' '.join(words[1:])
+        irc.reply(r2.response(encode('utf-8', 'ignore')))
+        irc.reply('YEAAAAAAAAAAAAAAAAAAAAAAH')
+
     def pairtree(self, irc, msg, args, id):
         """<id>
         Generate a pairtree based on the given ID.
