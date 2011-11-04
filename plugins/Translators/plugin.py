eb1ed58190e5ab572526342bb623fd4d5e64248f
Michael Klein <mbklein@gmail.com>
2011-11-04 13:47:58 -0700

    @rob

--- plugins/Translators/plugin.py
+++ plugins/Translators/plugin.py
@@ -108,7 +108,13 @@ class Translators(callbacks.Privmsg):
         """
         irc.reply("nosrsly, " + ' '.join(args), prefixNick=True)
 
-    
+    def rob(self, irc, msg, args):
+        """
+        Rob says what you want him to say. Kinda.
+        """
+        phrase = args[0:randint(len(args)-1)]
+        irc.reply(' '.join(phrase) + '... Wait, what?')
+
     def obamit(self, irc, msg, args):
         """
         Garners attention for your statements in a folksy way
